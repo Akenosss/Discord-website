eb00d61d91dd99004847903bb0b6c96c12014b68
Leo <ribotleo@gmail.com>
2023-09-13 16:06:02 +0200

    feat(authentification) sign-in & sign-up

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import {UserButton} from "@clerk/nextjs";
+
 export default function Home() {
   return (
       <div className="">
-        This is a protected route.
+        <UserButton afterSignOutUrl="/"/>
       </div>
   )
 }
--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import React from "react";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -16,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+      <ClerkProvider>
+        <html lang="en">
+        <body className={font.className}>
+          {children}
+        </body>
+        </html>
+      </ClerkProvider>
   )
 }
